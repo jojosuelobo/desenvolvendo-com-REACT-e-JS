e21cfd7b6aa4549d0c3df858b8f9e223a333c8a4
jojosuelobo <josue.lobo@etaure.com.br>
2023-05-24 15:28:43 -0300

    correção

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -1,13 +1,12 @@
-import './ListaSuspensa.css';
+import './ListaSuspensa.css'
 
 const ListaSuspensa = (props) => {
-    
     return (
-        <div className="lista-suspensa">
-            <label> {props.label} </label>
-            <select onChange={evento => (evento.target.value)} value={props.value} required={props.obrigatorio}>
-                {props.itens.map(item => { 
-                return <option key={item}>{item}</option> 
+        <div className='lista-suspensa'>
+            <label>{props.label}</label>
+            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.required} value={props.value}>
+                {props.itens.map(item => {
+                    return <option key={item}>{item}</option>
                 })}
             </select>
         </div>
