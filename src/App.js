e0c1dfdde1967797b51ce261a9ad50a0bc058260
jojosuelobo <josue.lobo@etaure.com.br>
2023-05-25 13:37:42 -0300

    Topico 4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import Banner from './componentes/Banner/index';
-import CampoTexto from './componentes/CampoTexto';
+//import CampoTexto from './componentes/CampoTexto';
 import Formulario from './componentes/Formulario';
 import { useState } from 'react';
+import Time from './componentes/Time';
 
 function App() {
+
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57C279',
+      corSecundaria: '#D9F7E9'
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF'
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2'
+    },
+    {
+      nome: 'DevOps',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8'
+    },
+  ]
 
   const [colaboradores, setColaboradores] = useState([])
 
@@ -15,7 +39,19 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionar(colaborador)}/>
+      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionar(colaborador)}/>
+      
+       {times.map(time => <Time 
+       key={time.nome} 
+       nome={time.nome} 
+       corPrimaria={time.corPrimaria} 
+       corSecundaria={time.corSecundaria} 
+       colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+       />)} 
+    
+
+      
+
     </div>
   );
 }
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -6,13 +6,7 @@ import './Formulario.css'
 
 const Formulario = (props) => {
 
-    const times = [
-        'Programação',
-        'Front-End',
-        'Data Science',
-        'DevOps',
-        'Back-End'
-    ]
+
 
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
@@ -28,6 +22,10 @@ const Formulario = (props) => {
             imagem,
             time
         })
+        setNome('')
+        setCargo('')
+        setImagem('')
+        setTime('')
     }
 
     return (
@@ -37,7 +35,7 @@ const Formulario = (props) => {
                 <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" valor={nome} aoAlterado={valor => setNome(valor)} />
                 <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" valor={cargo} aoAlterado={valor => setCargo(valor)} />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" valor={imagem} aoAlterado={valor => setImagem(valor)} />
-                <ListaSuspensa aoAlterado={valor => setTime(valor)} valor={time} obrigatorio={true} label="Time" itens={times} />
+                <ListaSuspensa aoAlterado={valor => setTime(valor)} valor={time} obrigatorio={true} label="Time" itens={props.times} />
                 <Botao>Criar Card</Botao>
             </form>
         </section>
